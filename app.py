058c835447b1d24084816842f7dec5a8e4108307
Yuichi Ogino <yuichi.ogino@sansan.com>
2021-04-22 20:00:07 +0900

    extract module

--- app.py
+++ app.py
@@ -1,40 +1,20 @@
 """Main Python application file for the EEL-CRA demo."""
 
-import os
 import platform
-import random
 import sys
 
 import eel
+from api import *
 
 # Use latest version of Eel from parent directory
 sys.path.insert(1, '../../')
 
 
-@eel.expose  # Expose function to JavaScript
-def say_hello_py(x):
-    """Print message from JavaScript on app initialization, then call a JS function."""
-    print('Hello from %s' % x)  # noqa T001
-    eel.say_hello_js('Python {from within say_hello_py()}!')
-
-
-@eel.expose
-def expand_user(folder):
-    """Return the full path to display in the UI."""
-    return '{}/*'.format(os.path.expanduser(folder))
-
-
-@eel.expose
-def pick_file(folder):
-    """Return a random file from the specified folder."""
-    folder = os.path.expanduser(folder)
-    if os.path.isdir(folder):
-        listFiles = [_f for _f in os.listdir(folder) if not os.path.isdir(os.path.join(folder, _f))]
-        if len(listFiles) == 0:
-            return 'No Files found in {}'.format(folder)
-        return random.choice(listFiles)
-    else:
-        return '{} is not a valid folder'.format(folder)
+# @eel.expose  # Expose function to JavaScript
+# def say_hello_py(x):
+#     """Print message from JavaScript on app initialization, then call a JS function."""
+#     print('Hello from %s' % x)  # noqa T001
+#     eel.say_hello_js('Python {from within say_hello_py()}!')
 
 
 def start_eel(develop):
