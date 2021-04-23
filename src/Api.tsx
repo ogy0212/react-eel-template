20f252d8cda5b767e392a7760986392a40c89591
Yuichi Ogino <yuichi.ogino@sansan.com>
2021-04-23 21:09:13 +0900

    extract api from component

---
+++ src/Api.tsx
@@ -0,0 +1,26 @@
+// Point Eel web socket to the instance
+export const eel = window.eel
+eel.set_host('ws://localhost:8080')
+
+// Expose the `sayHelloJS` function to Python as `say_hello_js`
+const sayHelloJS = (x: any) => {
+    console.log('Hello from ' + x)
+}
+// WARN: must use window.eel to keep parse-able eel.expose{...}
+window.eel.expose(sayHelloJS, 'say_hello_js')
+
+// Test anonymous function when minimized. See https://github.com/samuelhwilliams/Eel/issues/363
+const show_log = (msg: string) => {
+    console.log(msg)
+}
+window.eel.expose(show_log, 'show_log')
+
+// Test calling sayHelloJS, then call the corresponding Python function
+sayHelloJS('Javascript World!')
+eel.say_hello_py('Javascript World!')
+
+export const InitApi = () => {
+    console.log('Api Ready!')
+}
+
+export default InitApi;
--- src/pick-file/PickFile.tsx
+++ src/pick-file/PickFile.tsx
@@ -3,24 +3,6 @@ import './PickFile.css';
 
 // Point Eel web socket to the instance
 export const eel = window.eel
-eel.set_host('ws://localhost:8080')
-
-// Expose the `sayHelloJS` function to Python as `say_hello_js`
-const sayHelloJS = (x: any) => {
-    console.log('Hello from ' + x)
-}
-// WARN: must use window.eel to keep parse-able eel.expose{...}
-window.eel.expose(sayHelloJS, 'say_hello_js')
-
-// Test anonymous function when minimized. See https://github.com/samuelhwilliams/Eel/issues/363
-const show_log = (msg: string) => {
-    console.log(msg)
-}
-window.eel.expose(show_log, 'show_log')
-
-// Test calling sayHelloJS, then call the corresponding Python function
-sayHelloJS('Javascript World!')
-eel.say_hello_py('Javascript World!')
 
 // Set the default path. Would be a text input, but this is a basic example after all
 const defPath = '~'
