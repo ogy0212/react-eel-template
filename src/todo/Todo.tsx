40a9ea5a522a9215f0b97fc8915d857c80577360
ogy <ba550b0e@gmail.com>
2021-05-09 15:43:33 +0900

    style

--- src/todo/Todo.tsx
+++ src/todo/Todo.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import DeleteIcon from '@material-ui/icons/Delete';
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from '@material-ui/core/IconButton';
 
-export const Todo = ({todo, deleteTodo}: {todo: any, deleteTodo: any}) => {
-
+export const Todo = ({ todo, deleteTodo }: { todo: any; deleteTodo: any }) => {
     return (
         <ListItem button>
-            <ListItemIcon>
-                <DeleteIcon onClick={() => deleteTodo(todo.id)} />
-            </ListItemIcon>
             <ListItemText primary={todo.text} />
+            <ListItemSecondaryAction>
+                <IconButton edge="end" aria-label="delete" onClick={() => deleteTodo(todo.id)}>
+                    <DeleteIcon />
+                </IconButton>
+            </ListItemSecondaryAction>
         </ListItem>
     );
-}
+};
 
 export default Todo;
