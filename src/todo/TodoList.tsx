24f033e163cc3c97922404372fc2bdd0cd9bb793
ogy <ba550b0e@gmail.com>
2021-05-09 21:36:05 +0900

    text input controll

--- src/todo/TodoList.tsx
+++ src/todo/TodoList.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import eel from '../Api';
-
-import Todo from './Todo';
-
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import List from '@material-ui/core/List';
+import Todo from './Todo';
 
 export const TodoList = () => {
+    const [inputText, setInputText] = useState<string>('');
     const [todoList, setTodoList]: [any, any] = useState([]);
+
+    const handleChange = (e: any) => {
+        if (!!e && !!e.target) {
+            setInputText(e.target.value);
+        }
+    }
     
     const getAllTodo = () => {
         eel.get_all_todo()((resp: any) => {
@@ -16,13 +21,17 @@ export const TodoList = () => {
         });
     }
 
-    const addTodo = (text: string) => {
-        eel.add_todo(text)(() =>{
+    const addTodo = () => {
+        if (!inputText.trim()){
+            return;
+        }
+        eel.add_todo(inputText.trim())(() =>{
             getAllTodo();
+            setInputText((_: string) => '');
         });
     }
 
-    const deleteTodo = (id: any) => {
+    const deleteTodo = (id: number) => {
         eel.delete_todo(id)(() => {
             getAllTodo();
         });
@@ -35,8 +44,8 @@ export const TodoList = () => {
     return (
         <>
             <div>
-                <TextField style={{ display: 'inline' }} id="standard-basic" label="Add Item Here." />
-                <Button style={{ display: 'inline' }} color="primary" variant="contained" onClick={() => addTodo('piyo')}>Add</Button>
+                <TextField style={{ display: 'inline' }} id="standard-basic" label="Add Item Here." value={inputText} onChange={e => handleChange(e)} />
+                <Button style={{ display: 'inline' }} color="primary" variant="contained" onClick={() => addTodo()}>Add</Button>
             </div>
             <List component="nav" aria-label="main mailbox folders">
                 {todoList.map((todo: any) => (
